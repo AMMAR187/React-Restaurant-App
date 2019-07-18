4ba67c4e94f6dba06785859d7e4baa06cd59a2c8
Ammar Dafaalla Mahmoud <mr.ammar.96@gmail.com>
2019-07-18 19:46:33 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,73 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle, Media } from 'reactstrap';
 
-class Dishdetail extends Component {
-    constructor(props) {
-        super(props);
 
+function RenderDish({ dish }) {
+    console.log(dish);
+
+    if (dish != null && typeof (dish) !== 'undefined') {
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText >{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    } else {
+        return (<div></div>);
     }
-    renderComments(Dish) {
-        var Comments = [];
-        if (typeof (Dish) !== 'undefined') {
-            Dish.comments.map((comment) => {
-                Comments.push(
-                    <Media as="li" key={comment.id}>
-                        <Media body>
-                            <p>{comment.comment}</p>
-                            <p>--{comment.author} ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                        </Media>
+}
+function RenderComments({ dish }) {
+    var Comments = [];
+    if (dish != null && typeof (dish) !== 'undefined') {
+        dish.comments.map((comment) => {
+            Comments.push(
+                <Media as="li" key={comment.id}>
+                    <Media body>
+                        <p>{comment.comment}</p>
+                        <p>--{comment.author} ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                     </Media>
-                );
-            });
-            return (
-                <div className="col-12 col-md-5 m1">
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled" >
-                        {Comments}
-                    </ul>
-                </div>
-
+                </Media>
             );
-        }
-    }
-    render() {
-
-        var Dishdetail = [];
-        var dish = [];
-        if (this.props.dish) {
-            Dishdetail = this.props.dish;
-            dish =
-                <Card>
-                    <CardImg top src={Dishdetail.image} alt={Dishdetail.name} />
-                    <CardBody>
-                        <CardTitle>{Dishdetail.name}</CardTitle>
-                        <CardText >{Dishdetail.description}</CardText>
-                    </CardBody>
-                </Card>;
-        }
-
-
-
-
-
-
-
-
+        });
 
         return (
-            <div className="container text-left">
-                <div className="row">
-                    <div className="col-12 col-md-5 m1">
-                        {dish}
-                    </div>
-                    {this.renderComments(this.props.dish)}
-                </div>
+            <div className="col-12 col-md-5 m1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled" >
+
+                    {Comments}
+                </ul>
             </div>
-
-
-
         );
+    } else {
+        return (<div></div>);
     }
+
+}
+
+const Dishdetail = (props) => {
+    return (
+        <div className="container text-left">
+            <div className="row">
+                <div className="col-12 col-md-5 m1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <RenderComments dish={props.dish} />
+            </div>
+        </div>
+    );
 }
 
 export default Dishdetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,46 +1,32 @@
-import React, { Component } from 'react';
-import { Media } from "reactstrap";
+import React from 'react';
 import { Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from 'reactstrap';
-import Dishdeatail from './DishdetailComponent';
 
-class Menu extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDish: null
-        };
-    }
+function RenderMenuItem({ dish, onClick }) {
 
-
-    // renderDish(dish) {
-    //     if (dish != null) {
-    //         return (
-    //             <Dishdeatail dish={dish} />
-    //         );
-    //     } else {
-    //         return (<div></div>);
-    //     }
-    // }
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 col-md-5 m-1" >
-                    <Card onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay >
-                            <CardTitle >{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        })
+    return (
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay >
+                <CardTitle >{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => {
         return (
-            <div className="container text-left">
-                <div className="row">
-                    {menu}
-                </div>
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+    return (
+        <div className="container text-left">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
 }
+
 export default Menu;
