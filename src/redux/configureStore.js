11e514f0a4f5f7003f026feaa1519c9186acd30b
Ammar Dafaalla Mahmoud <mr.ammar.96@gmail.com>
2019-08-04 17:01:16 +0530

    Intro. to Redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import { initialState, Reducer } from './reducer';
+import { createStore } from 'redux';
+
+export const ConfigureStore = () => {
+    const store = createStore(Reducer, initialState);
+    return store;
+}
